facb774da7b495a26f1e53865da30e9d3a266245
Kai <riselikethemoon@gmail.com>
2016-02-16 15:30:53 -0800

    added ability to request list of languages

--- server.py
+++ server.py
@@ -7,18 +7,32 @@ api = Api(app)
 
 replies = {
     "english": ["yes", "no", "I think so"],
-    "spanish": ["si", "no", "creo que si"]
+    "spanish": ["si", "no", "creo que si"],
+    "cat": ["purrrfectly possible", "hiss no", "mouse certainly"],
+    "french": ["a chaque jour suffit sa peine", "non", "c'est kif-kif et bourricot",
+                "ce ne sont pas vos oignons", "la mort du petit cheval",
+                "qui vivra, verra", "sans l'ombre d'un doute",
+                "triste comme un repas sans fromage"]
 }
 
 class Magic(Resource):
-
     def get(self, language="english"):
+        """Given a language (english is default), returns random reply"""
 
         reply = random.choice(replies[language])
         return {"reply": reply}
 
+class Language(Resource):
+    def get(self):
+        """Returns a list of available languages"""
+        
+        languages = replies.keys()
+        return {"languages": languages}
+
+
 
 api.add_resource(Magic, "/", "/<string:language>")
+api.add_resource(Language, "/languages")
 
 
 if __name__ == "__main__":
