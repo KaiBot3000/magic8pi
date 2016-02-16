0783cc54a615fdbe5c2b4563ed671ca94e27618f
Kai <riselikethemoon@gmail.com>
2016-02-16 14:57:00 -0800

    returning random english fortune

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import random
 
 app = Flask(__name__)
 api = Api(app)
 
+fortunes = {
+    "english": ["yes", "no", "I think so"],
+    # "spanish": ["si", "no", "creo que si"]
+}
+
 class Magic(Resource):
     def get(self):
-        return {"yes": "it worked"}
+        fortune = random.choice(fortunes["english"])
+        return {"fortune": fortune}
 
 api.add_resource(Magic, "/")
 
